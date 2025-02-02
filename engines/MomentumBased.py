35bc258c57a9f844963ce594813f06a638df5bb5
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2025-02-02 16:23:34 -0500

    Create MomentumBased.py

---
+++ engines/MomentumBased.py
@@ -0,0 +1,23 @@
+class MomentumBased:
+    def __init__(self):
+        self.points = 0
+        self.streak_count = 0
+        self.current_streak = True
+        
+    def move(self, op_last_move):
+        if op_last_move == self.current_streak:
+            self.streak_count += 1
+        else:
+            self.streak_count = 1
+            self.current_streak = op_last_move
+            
+        if self.streak_count >= 3:
+            result = self.current_streak
+        else:
+            result = not self.current_streak
+            
+        print(f"MomentumBased: {result}")
+        return result
+        
+    def __repr__(self):
+        return "MomentumBased"
