4ebd4e81306686cc81a2d7143eed478e2cbc022d
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2025-02-02 16:10:35 -0500

    Create GradualTitForTat.py

---
+++ engines/GradualTitForTat.py
@@ -0,0 +1,26 @@
+class GradualTitForTat:
+    def __init__(self):
+        self.points = 0
+        self.defection_count = 0
+        self.punishment_mode = False
+        self.punishment_counter = 0
+        
+    def move(self, op_last_move):
+        if not op_last_move:
+            self.defection_count += 1
+            self.punishment_mode = True
+            self.punishment_counter = self.defection_count
+        
+        if self.punishment_mode:
+            if self.punishment_counter > 0:
+                self.punishment_counter -= 1
+                print("GradualTitForTat: False")
+                return False
+            else:
+                self.punishment_mode = False
+        
+        print("GradualTitForTat: True")
+        return True
+        
+    def __repr__(self):
+        return "GradualTitForTat"
