8bc7bab267afbe861a4851ecf03d67ab5bc965de
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2025-02-02 16:24:51 -0500

    Create GradualLearner.py

---
+++ engines/GradualLearner.py
@@ -0,0 +1,19 @@
+class GradualLearner:
+    def __init__(self):
+        self.points = 0
+        self.learning_rate = 0.1
+        self.cooperation_probability = 0.5
+        
+    def move(self, op_last_move):
+        if op_last_move:
+            self.cooperation_probability += (1 - self.cooperation_probability) * self.learning_rate
+        else:
+            self.cooperation_probability -= self.cooperation_probability * self.learning_rate
+            
+        import random
+        result = random.random() < self.cooperation_probability
+        print(f"GradualLearner: {result}")
+        return result
+        
+    def __repr__(self):
+        return "GradualLearner"
