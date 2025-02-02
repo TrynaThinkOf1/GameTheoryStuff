32baa9b06e27613dbfeb6962d78905a38d6d8820
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2025-02-02 16:26:56 -0500

    Create ProbabilisticMimicry.py

---
+++ engines/ProbabilisticMimicry.py
@@ -0,0 +1,20 @@
+class ProbabilisticMimicry:
+    def __init__(self):
+        self.points = 0
+        self.copy_probability = 0.7
+        self.default_move = True
+        import random
+        self.random = random
+        
+    def move(self, op_last_move):
+        if self.random.random() < self.copy_probability:
+            result = op_last_move
+        else:
+            result = self.default_move
+            self.default_move = not self.default_move
+            
+        print(f"ProbabilisticMimicry: {result}")
+        return result
+        
+    def __repr__(self):
+        return "ProbabilisticMimicry"
