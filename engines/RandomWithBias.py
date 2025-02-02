df659a667b9787c767435a5a2c62ddd5fdae0f17
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2025-02-02 16:10:10 -0500

    Create RandomWithBias.py

---
+++ engines/RandomWithBias.py
@@ -0,0 +1,14 @@
+class RandomWithBias:
+    def __init__(self):
+        self.points = 0
+        import random
+        self.random = random
+        self.cooperation_bias = 0.7
+        
+    def move(self, op_last_move):
+        result = self.random.random() < self.cooperation_bias
+        print(f"RandomWithBias: {result}")
+        return result
+        
+    def __repr__(self):
+        return "RandomWithBias"
