74c9d60abc85b605d4a2ff41a62b6649ed2335c1
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2025-02-02 16:12:22 -0500

    Create TwoTitsForTat.py

---
+++ engines/TwoTitsForTat.py
@@ -0,0 +1,17 @@
+class TwoTitsForTat:
+    def __init__(self):
+        self.points = 0
+        self.last_two_moves = [True, True]
+        
+    def move(self, op_last_move):
+        self.last_two_moves.append(op_last_move)
+        self.last_two_moves.pop(0)
+        
+        if not all(self.last_two_moves):
+            print("TwoTitsForTat: False")
+            return False
+        print("TwoTitsForTat: True")
+        return True
+        
+    def __repr__(self):
+        return "TwoTitsForTat"
