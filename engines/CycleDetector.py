5d37bb3c17ce9d3cef60b799b1a229112b71ad62
TrynaThinkOf1 <89053300+TrynaThinkOf1@users.noreply.github.com>
2025-02-02 16:26:17 -0500

    Create CycleDetector.py

---
+++ engines/CycleDetector.py
@@ -0,0 +1,30 @@
+class CycleDetector:
+    def __init__(self):
+        self.points = 0
+        self.history = []
+        self.min_cycle_length = 2
+        self.max_cycle_length = 4
+        
+    def find_cycle(self):
+        for length in range(self.min_cycle_length, min(self.max_cycle_length + 1, len(self.history))):
+            recent = self.history[-length:]
+            previous = self.history[-2*length:-length]
+            if recent == previous:
+                return length, recent
+        return None, None
+        
+    def move(self, op_last_move):
+        self.history.append(op_last_move)
+        cycle_length, pattern = self.find_cycle()
+        
+        if cycle_length:
+            next_predicted = pattern[0]
+            result = not next_predicted
+        else:
+            result = True
+            
+        print(f"CycleDetector: {result}")
+        return result
+        
+    def __repr__(self):
+        return "CycleDetector"
